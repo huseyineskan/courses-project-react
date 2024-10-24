670838492f92cc1919e500913a50e17d647d03c6
Huseyin Eskan <huseyineskan01@gmail.com>
2024-10-24 17:08:23 +0200

    added new course

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -26,7 +26,7 @@ export const courses = [
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     price: 20,
-    img: "../src/img/3.png",
+    img: "../src/img/1.png",
     link: "https://youtube.com",
   },
   {
@@ -56,7 +56,7 @@ export const courses = [
     description:
       "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
     price: 70,
-    img: "../src/img/2.png",
+    img: "../src/img/1.png",
     link: "https://youtube.com",
   },
 ];
